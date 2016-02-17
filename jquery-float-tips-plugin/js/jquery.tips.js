9ecf6dd840087ad0f298b79b5a124cdb0f44e862
laike <lake1355@qq.com>
2016-02-17 17:47:59 +0800

    modified script

--- jquery-float-tips-plugin/js/jquery.tips.js
+++ jquery-float-tips-plugin/js/jquery.tips.js
@@ -2,7 +2,28 @@
   $.fn.tips=function(options){
        
        var opts = $.extend({}, $.fn.tips.default, options);
-       debug('ok');
+       var $this = $(this);
+       var _header = $this.find('.jq-tips-header');
+       var _content = $this.find('.jq-tips-content');
+       var _min_btn = $this.find('.jq-tips-mini');
+       var _close_btn = $this.find('.jq-tips-close');
+       if(opts.bottom){
+       	 if(typeof opts.bottom == 'string'){
+             $this.css({bottom:opts.bottom});
+       	 }else if(typeof opts.bottom == 'number'){
+               $this.css({bottom:opts.bottom+'px'});
+       	 }
+       }
+       if(opts.right){
+       	 if(typeof opts.right == 'string'){
+             $this.css({right:opts.right});
+       	 }else if(typeof opts.right == 'number'){
+               $this.css({right:opts.right+'px'});
+       	 }
+       }
+       //是否可以拖动
+       
+       debug($this);
        
   }
   function debug(info){
@@ -19,6 +40,7 @@
   $.fn.tips.default = {
   	 isshowcolsebtn:true,
   	 canminum:true,
-  	 debugmode:true
+  	 debugmode:true,
+  	 candrag:true
   };
 })(jQuery)
