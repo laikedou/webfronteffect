5c3a03575abf9d7a96401a347f600a35d1cbac26
laike <lake1355@qq.com>
2016-02-18 12:46:56 +0800

    this is the first version of the plugin

--- jquery-float-tips-plugin/js/jquery.tips.js
+++ jquery-float-tips-plugin/js/jquery.tips.js
@@ -7,6 +7,7 @@
        var _content = $this.find('.jq-tips-content');
        var _min_btn = $this.find('.jq-tips-mini');
        var _close_btn = $this.find('.jq-tips-close');
+       
        if(opts.bottom){
        	 if(typeof opts.bottom == 'string'){
              $this.css({bottom:opts.bottom});
@@ -21,11 +22,112 @@
                $this.css({right:opts.right+'px'});
        	 }
        }
+       function getScrollOffsets(w){
+       	var w = w || window;
+       	if(w.pageXOffset != null){
+           return {
+           	 x : w.pageXOffset,
+           	 y : w.pageYOffset
+           }
+       	}
+       	var d = w.document;
+       	if(document.compatMode == 'CSS1Compat'){
+       		return {
+       			x : d.documentElement.scrollLeft,
+       			y : d.documentElement.scrollTop
+       		}
+       	}
+
+       	return {
+       		 x:d.body.scrollLeft,
+       		 y:d.body.scrollTop
+       	}
+       }
+       function drag(event){
+       	      event = event || window.event;
+              var drag = true;
+              var elementToDrag = this;
+              var scroll = getScrollOffsets();
+              var startX  = event.clientX + scroll.x;
+              var startY = event.clientY + scroll.y;
+              var origX = elementToDrag.offsetLeft;
+              var origY = elementToDrag.offsetTop;
+              var deltaX = startX - origX;
+              var deltaY = startY - origY;
+              var drag_header = _header[0];
+
+              if(document.addEventListener){
+              	document.addEventListener('mousemove',moveHandler,true);
+              	document.addEventListener('mouseup',upHandler,true);
+              	drag_header.addEventListener('mouseout',function(e){
+                        drag = false;
+              	});
+              	drag_header.addEventListener('mousedown',function(e){
+              		    drag = true;
+              	});
+              }else if(document.attachEvent){
+              	//捕获事件
+              	elementToDrag.setCapture();
+                elementToDrag.attachEvent('onmousemove',moveHandler);
+                elementToDrag.attachEvent('onmouseup',upHandler);
+                drag_header.attachEvent('onmouseout',function(e){
+                        drag = false;
+              	});
+              	drag_header.attachEvent('onmousedown',function(e){
+              		    drag = true;
+              	});
+                elementToDrag.attachEvent('onlosecapture',upHandler);
+              }
+              //阻止冒泡
+              if(event.stopPropagation) event.stopPropagation();
+              else event.cancelBubble = true;
+              //阻止默认事件
+              if(event.preventDefault) event.preventDefault();
+              else event.returnValue = false;
+
+              function moveHandler(e){
+              	if(!drag){
+                   return;
+              	}
+                e = e || window.event;
+                var scroll = getScrollOffsets();
+                elementToDrag.style.left = (e.clientX + scroll.x - deltaX) + 'px';
+                elementToDrag.style.top = (e.clientY + scroll.y - deltaY) + 'px';
+                if(event.stopPropagation) event.stopPropagation();
+                else event.cancelBubble = true;
+              } 
+
+              function upHandler(e){
+              	drag = false;
+                e = e || window.event;
+                if(document.removeEventListener){
+                	  document.removeEventListener('mousemove', moveHandler);
+                	  document.removeEventListener('mouseup', upHandler);
+                }else if(document.detachEvent){
+                	  document.detachEvent('onlosecapture',upHandler);
+                	  document.detachEvent('onmouseup',upHandler);
+                	  document.detachEvent('onmousemove',moveHandler);
+                	  elementToDrag.releaseCapture();
+                	  if(event.stopPropagation) event.stopPropagation();
+                      else event.cancelBubble = true;
+                }
+              }
+       }
        //是否可以拖动
-       
-       debug($this);
+       if(opts.candrag){
+           $this.bind('mousedown',drag);
+       }
+       //绑定关闭和缩小事件
+       _close_btn.click(function(e){
+           e.preventDefault();
+           $this.fadeOut(500);
+       });
+       _min_btn.click(function(e){
+       	   _content.toggle('slow')
+       });
        
   }
+
   function debug(info){
      try {
      	// statements
@@ -40,7 +142,7 @@
   $.fn.tips.default = {
   	 isshowcolsebtn:true,
   	 canminum:true,
-  	 debugmode:true,
+  	 debugmode:false,
   	 candrag:true
   };
 })(jQuery)
