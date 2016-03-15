97e077117f9d305b05f5eec5b31db0c41cd8e01b
laike <lake1355@qq.com>
2016-03-15 17:50:42 +0800

    modified http.js

--- webtools/js/http.js
+++ webtools/js/http.js
@@ -150,6 +150,7 @@ function checkBrowserIsSupportProgressEvent(){
   }
 }
 whenReady(function(){
+  /*
   var elts = document.getElementsByTagName('input');
   for (var i = 0; i < elts.length; i++) {
     var input=elts[i];
@@ -163,7 +164,77 @@ whenReady(function(){
       xhr.open('POST',url);
       xhr.send(file);
     },false);
-  }
+  }*/
   //postFormData('http://www.baidu.com');
-  checkBrowserIsSupportProgressEvent();//检测当前浏览器对http progress事件的支持情况！
+  //checkBrowserIsSupportProgressEvent();//检测当前浏览器对http progress事件的支持情况！
+  
+  var elts =document.getElementsByClassName('fileDropTarget');
+  for (var i = 0; i < elts.length; i++) {
+    var target = elts[i];
+    var url = target.getAttribute('data-uploadto');
+    if(!url) continue;
+    createFileUploadDropTarget(target,url);
+  }
+  function createFileUploadDropTarget(target,url){
+    //跟踪当前是否上传，因此我们能拒绝放下
+    //我们可以处理多个并发上传
+    //但对这个例子使用进度通知太困难了
+   var uploading = false;
+   console.log(target,url);
+   target.ondragenter = function(e){
+      console.log('dragenter');
+      if(uploading) return;//如果正在忙，忽略拖放
+      var types = e.dataTransfer.types;
+      if(types && types.contains && types.contains('files') ||(types.indexOf && types.indexOf('files') !== -1)){
+                 target.classList.add('wantdrop');
+                 return false;
+      }
+  };
+  target.ondragover = function(e){
+    if(!uploading) return false;
+  };
+  target.ondragleave=function(e){
+    if(!uploading) target.classList.remove('wantdrop');
+  };
+  target.ondrop=function(e){
+    if(uploading) return false;
+    var files = e.dataTransfer.files;
+    if(files && files.length){
+      uploading = true;
+      var message = 'uploading files : <ul>';
+      for (var i = 0; i < files.length; i++) {
+         message += '<li>'+files[i].name+'</li>'
+      }
+      message += '</ul>';
+      target.innerHTML = message;
+      target.classList.remove('wantdrop');
+      target.classList.add('uploading');
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST',url);
+      var body = new FormData();
+      for (var i = 0; i < files.length; i++) {
+        body.append(i,files[i]);
+      }
+      xhr.upload.onprogress = function(e){
+                if(e.lengthComputable){
+                   target.innerHTML = message + Math.round(e.loaded/e.total*100)+"% Complete";
+                }
+
+      };
+      xhr.upload.onload = function(e){
+      uploading = false;
+      target.classList.remove('uploading');
+      target.innerHTML = 'Dorp Files  to upload!';
+      }
+      xhr.send(body);
+      return false;
+    }
+    target.classList.remove('wantdrop');
+    
+  }
+
+
+  }
+
+
 });
